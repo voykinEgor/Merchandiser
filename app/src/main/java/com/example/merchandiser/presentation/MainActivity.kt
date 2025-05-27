9829213043c8e668c99efd6b41dd2e39e403d266
voiki <voikin.egor2@gmail.com>
2025-05-27 15:41:42 +0300

    Добавлен фрагмент авторизации.

--- app/src/main/java/com/example/merchandiser/presentation/MainActivity.kt
+++ app/src/main/java/com/example/merchandiser/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.merchandiser
+package com.example.merchandiser.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.merchandiser.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/merchandiser/presentation/auth/AuthViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.merchandiser.presentation.auth
+
+class AuthViewModel {
+}
