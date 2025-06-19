006f32cc4b6776ac2d74fa9118476d3214ef169e
Alexey Bukharin <alesh-buk@yandex.ru>
2025-06-19 11:43:05 +0300

    fix: Добавил зависимости в Dagger

--- app/src/main/java/com/example/merchandiser/di/DataModule.kt
+++ app/src/main/java/com/example/merchandiser/di/DataModule.kt
@@ -3,9 +3,11 @@ package com.example.merchandiser.di
 import com.example.merchandiser.data.ApiFactory
 import com.example.merchandiser.data.ApiService
 import com.example.merchandiser.data.repositoriesImpls.AuthRepositoryImpl
+import com.example.merchandiser.data.repositoriesImpls.CategoryRepositoryImpl
 import com.example.merchandiser.data.repositoriesImpls.ShopRepositoryImpl
 import com.example.merchandiser.data.repositoriesImpls.TaskRepositoryImpl
 import com.example.merchandiser.domain.repositories.AuthRepository
+import com.example.merchandiser.domain.repositories.CategoryRepository
 import com.example.merchandiser.domain.repositories.ShopRepository
 import com.example.merchandiser.domain.repositories.TaskRepository
 import dagger.Binds
@@ -26,6 +28,10 @@ abstract class DataModule {
     @Binds
     @ApplicationScope
     abstract fun bindTaskRepository(impl: TaskRepositoryImpl): TaskRepository
+
+    @Binds
+    @ApplicationScope
+    abstract fun bindCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository
 
     companion object {
         @Provides
